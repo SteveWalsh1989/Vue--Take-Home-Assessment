2f61af91dfeca97ed2211cd47ce0e4e97635289a
Steve Walsh <stevewalsh89@gmail.com>
2022-07-21 11:43:44 +0100

    add search term to empty state

--- src/components/EmptyState.vue
+++ src/components/EmptyState.vue
@@ -1,9 +1,11 @@
-<script setup></script>
+<script setup>
+import { search } from '@/composables/useSearch';
+</script>
 
 <template>
   <section class="flex w-full text-center justify-center p-12">
     <div class="flex-col">
-      <h1 class="font-semibold">No Results Found</h1>
+      <h1 class="font-semibold">No Results Found for: '{{ search.term}}'</h1>
       <h2>Please try with a new search term</h2>
     </div>
   </section>
